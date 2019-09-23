7c845ac5e9cd3615920c3e94807ecbe8d864aa0e
chikutaku <chikutaku.iphone.2017@gmail.com>
2019-09-23 22:46:18 +0900

    add websocket lib (Starscream)

--- rv2-ios-client/rv2-ios-client/ViewController.swift
+++ rv2-ios-client/rv2-ios-client/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import Starscream
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WebSocketDelegate {
+    func websocketDidConnect(socket: WebSocketClient) {
+        print("websocket is connected")
+    }
+    
+    func websocketDidDisconnect(socket: WebSocketClient, error: Error?) {
+        if let e = error as? WSError {
+            print("websocket is disconnected: \(e.message)")
+        } else if let e = error {
+            print("websocket is disconnected: \(e.localizedDescription)")
+        } else {
+            print("websocket disconnected")
+        }
+    }
+    
+    func websocketDidReceiveMessage(socket: WebSocketClient, text: String) {
+        print("Received text: \(text)")
+    }
+    
+    func websocketDidReceiveData(socket: WebSocketClient, data: Data) {
+        print("Received data: \(data.count)")
+    }
+    
+    var socket: WebSocket!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //socket = WebSocket(url: URL(string: "ws://localhost:8080/")!)
+        socket = WebSocket(url: URL(string: "wss://1frpd3s0zl.execute-api.ap-northeast-1.amazonaws.com/dev")!)
+        
+        socket.delegate = self
+        socket.connect()
     }
 
 
