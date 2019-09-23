d82e502a63a1e29df65dfef1e55a2ffc6914b91e
chikutaku <chikutaku.iphone.2017@gmail.com>
2019-09-23 11:49:44 +0900

    add ios client

---
+++ rv2-ios-client/rv2-ios-client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rv2-ios-client
+//
+//  Created by Takuya on 2019/09/22.
+//  Copyright © 2019年 Takuya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
